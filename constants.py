5baf3a28f266b3dae6e0ea7e412ebfbfb8801422
Omer Yacine <mariocynicys@gmail.com>
2023-07-06 20:40:10 +0300

    init all, sem = 1

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ import asyncio
 STORAGE_DIR = "vingine_data"
 """The directory in which non-db storable content are stored."""
 
-SEM = asyncio.Semaphore(10)
+SEM = asyncio.Semaphore(1)
 """A global semaphore to limit task concurrency."""
 
 WAIT_TO_INSPECT = False
